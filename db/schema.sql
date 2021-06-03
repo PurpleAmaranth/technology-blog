5de6138aa6b609bfcb3e577c6a7d6e7e48e6e430
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2021-06-02 18:23:00 -0600

    Added seed for database set up with sequelize.

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- sequelize does not use a schema
+-- create the "schema" in code which will be initialized in javascript
+
+DROP DATABASE IF EXISTS user_db;
+CREATE DATABASE user_db;
---
+++ models/index.js
@@ -0,0 +1,8 @@
+//default file for folder >>>index
+//node executes the file index first
+//launchpad for all other files in folder
+//each individual js in this folder is a table.
+
+const User = require('./User');
+
+module.exports = { User };
